8ec1d7366ec318bcb46e17ee14bf83ccd6df2de3
胡蕎伊 <www3391@gmail.com>
2017-08-10 18:24:14 +0800

    尚未成功

--- src/main/java/viewModel/AddViewModel.java
+++ src/main/java/viewModel/AddViewModel.java
@@ -1,6 +1,12 @@
 package viewModel;
 
+import java.util.Map;
+
+import org.zkoss.bind.Property;
+import org.zkoss.bind.ValidationContext;
+import org.zkoss.bind.Validator;
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.validator.AbstractValidator;
 
 import model.Car;
 import model.CarData;
@@ -10,8 +16,6 @@ public class AddViewModel {
 
     private Car car = new Car();
     private CarService carService = new CarData();
-
-
 
     public Car getCar() {
         return car;
@@ -25,4 +29,56 @@ public class AddViewModel {
         carService.insert(car);
     }
 
+    public Validator getModelValidator() {
+        System.out.println("ModelValidator有跑進來");
+        return new AbstractValidator() {
+            @Override
+            public void validate(ValidationContext ctx) {
+                Map<String,Property> beanProps = ctx.getProperties(ctx.getProperty().getBase());
+                String model = (String)beanProps.get("model").getValue();
+                if(model==null) {
+                    this.addInvalidMessage(ctx, "model", "請盡量描述車款詳情");
+                }
+            }
+        };
+    }
+    public Validator getMakeValidator() {
+        System.out.println("MakeValidator有跑進來");
+        return new AbstractValidator() {
+            @Override
+            public void validate(ValidationContext ctx) {
+                Map<String,Property> beanProps = ctx.getProperties(ctx.getProperty().getBase());
+                String make = (String)beanProps.get("make").getValue();
+                if(make==null) {
+                    this.addInvalidMessage(ctx, "make", "請盡量描述車款詳情");
+                }
+            }
+        };
+    }
+    public Validator getDespValidator() {
+        System.out.println("DespValidator有跑進來");
+        return new AbstractValidator() {
+            @Override
+            public void validate(ValidationContext ctx) {
+                Map<String,Property> beanProps = ctx.getProperties(ctx.getProperty().getBase());
+                String desp = (String)beanProps.get("desp").getValue();
+                if(desp==null) {
+                    this.addInvalidMessage(ctx, "desp", "請盡量描述車款詳情");
+                }
+            }
+        };
+    }
+    public Validator getPriceValidator() {
+        System.out.println("PriceValidator有跑進來");
+        return new AbstractValidator() {
+            @Override
+            public void validate(ValidationContext ctx) {
+                Map<String,Property> beanProps = ctx.getProperties(ctx.getProperty().getBase());
+                Integer price = (Integer)beanProps.get("price").getValue();
+                if(price <= 0||price==null) {
+                    this.addInvalidMessage(ctx, "price", "請輸入大於0的有效數字");
+                }
+            }
+        };
+    }
 }
